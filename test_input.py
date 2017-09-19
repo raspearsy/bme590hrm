d3f047c7c9d482b879ed25e1804365385e904325
ras93 <ras93@duke.edu>
2017-09-19 15:53:44 -0400

    small changes to input unit tests

--- test_input.py
+++ test_input.py
@@ -5,7 +5,7 @@ def test_isfile()
     assert os.path.isfile(ecgdata)
 
 def test_hrrawdatasize()
-    assert hrrawdata.amount() == 3
+    assert hrrawdata.amount() == 2
 
 def test_hrrawdatatype()
     assert type(hrrawdata[1]) is float or int
