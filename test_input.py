8464a66a83a9e30fbb6bc38e07c72e8c04e798e2
ras93 <ras93@duke.edu>
2017-09-19 15:53:44 -0400

    small changes to input unit tests

--- test_input.py
+++ test_input.py
@@ -6,7 +6,7 @@ def test_isfile()
 
 
 def test_hrrawdatasize()
-    assert hrrawdata.amount() == 3
+    assert hrrawdata.amount() == 2
 
 def test_hrrawdatatype()
     assert type(hrrawdata[1]) is float or int
