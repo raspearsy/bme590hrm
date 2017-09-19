29b7ea6255ad9a89ca45ca217168e931861779bd
ras93 <ras93@duke.edu>
2017-09-19 13:49:25 -0400

    preliminary input unit tests

---
+++ test_input.py
@@ -0,0 +1,13 @@
+import input
+
+def test_isfile()
+    import os.path
+    assert os.path.isfile(ecgdata)
+
+
+def test_hrrawdatasize()
+    assert hrrawdata.amount() == 3
+
+def test_hrrawdatatype()
+    assert type(hrrawdata[1]) is float or int
+    assert type(hrrawdata[2]) is float or int
