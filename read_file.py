25b9ff605ad8a671c79ae7b0c591a0cf32cd7789
Nisarg Shah <nisarg1shah@gmail.com>
2017-09-19 15:56:23 -0400

    Fixed error in path resulting in the wrong file being pushed

--- read_file.py
+++ read_file.py
@@ -1,1 +1,16 @@
-print(5)
+import pandas as pd
+
+# Input file as a Pandas DataFrame
+
+
+def input_dataframe(file):
+    """
+    :param file: The input file, ecg_data.csv
+    :return: An dataframe for the time and voltage ecg data
+    """
+
+    ecg_dataframe = pd.read_csv(file)
+    return ecg_dataframe
+
+if __name__ == "__main__":
+    input_dataframe("ecg_data.csv")
