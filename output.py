8eb0e7a279feff96ed7d752c1612b9127595df4d
Joshua Khani <jmk@duke.edu>
2017-09-20 11:11:12 -0400

    Made summarizeECG function in output.py

--- output.py
+++ output.py
@@ -1,18 +1,26 @@
-#Calls findInstHR() to get instantaneous heart rate
-instHR = findInstHR()
+def summarizeECG(threshold_hr, hr, brady, tachy):
+    """Create txt file summarizing ECG analysis
+    :param instHR: (int)
+    :param avgHR: (int)
+    :param brady: (int)
+    :param tachy: (int)
+    """
+    #Calls hrdetector() to get instantaneous heart rate
+    #instHR = findInstHR()
 
-#Calls findAvgHR() to get average heart rate
-avgHR = findAvgHR()
-#Calls bradyTimes() to get times when bradycardia occurred
-brady = bradyTimes()
-#Calls tachtimes() to get times when tachycardia occurred
-tachy = tachyTimes()
-#Writes the output of the ECG analysis to an output file named ecgOutput.txt
-ecgResults = open('ecgOutput.txt','w')
-instHRstr = "Estimated instantaneous heart rate: %s" % str(instHR)
-avgHRstr = "Estimated average heart rate: %s" % str(avgHR)
-bradystr = "Bradycardia occurred at: %s" % str(brady)
-tachystr = "Tachycardia occurred at: %s" % str(tachy)
+    #Calls findAvgHR() to get average heart rate
+    #avgHR = findAvgHR()
+    #Calls bradyTimes() to get times when bradycardia occurred
+    #brady = bradyTimes()
+    #Calls tachtimes() to get times when tachycardia occurred
+    #tachy = tachyTimes()
+    
+    #Writes the output of the ECG analysis to an output file named ecgOutput.txt
+    ecgResults = open('ecgOutput.txt','w')
+    instHRstr = "Estimated instantaneous heart rate: %s" % str(instHR)
+    avgHRstr = "Estimated average heart rate: %s" % str(avgHR)
+    bradystr = "Bradycardia occurred at: %s" % str(brady)
+    tachystr = "Tachycardia occurred at: %s" % str(tachy)
 
-ecgResults.write(instHRstr + '\n' + avgHRstr + '\n' + bradystr + '\n' + tachystr)
-ecgResults.close()
+    ecgResults.write(instHRstr + ' BPM\n' + avgHRstr + ' BPM\n' + bradystr + '\n' + tachystr)
+    ecgResults.close()
