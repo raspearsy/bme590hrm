a7f1b86b36d90301517bbe823736185e1ba7585a
ras93 <ras93@duke.edu>
2017-09-21 16:27:12 -0400

    added input unit tests

--- test_input.py
+++ test_input.py
@@ -1,12 +1,19 @@
+import os
+import pandas as pd
 import read_file.py
 
-def test_isfile()
-    import os.path
-    assert os.path.isfile(ecgdata)
+# requires test .csv file containing 2 columns w/ 1 row string header and all below rows float/int
+def test_ecg_dataframe_size():
+    ecg_dataframe=input_dataframe("testfile1.csv")
+    assert ecg_dataframe.shape[0]==2
 
-def test_hrrawdatasize()
-    assert hrrawdata.amount() == 2
+def test_ecg_dataframe_type():
+    assert isinstance(ecg_dataframe, pd.DataFrame)
+    assert isinstance(ecg_dataframe.voltage[0], float or int)
 
-def test_hrrawdatatype()
-    assert type(hrrawdata[1]) is float or int
-    assert type(hrrawdata[2]) is float or int
+def test_exception_nofile():
+
+def test_exception_emptyfile():
+
+def test_exception_nonnumeric_values():
+# need exceptions for file that doesn't exist; for empty file; for file w/ non-numeric values
