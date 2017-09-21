56b02ab51479b28f759973e543916215ad71adb4
Nisarg Shah <nisarg1shah@gmail.com>
2017-09-20 23:58:43 -0400

    Changed input parameter to be set to default file in the read_file module

--- read_file.py
+++ read_file.py
@@ -2,15 +2,13 @@ import pandas as pd
 
 # Input file as a Pandas DataFrame
 
-
-def input_dataframe(file):
+def input_dataframe(file="ecg_data.csv"):
     """
-    :param file: The input file, ecg_data.csv
     :return: An dataframe for the time and voltage ecg data
     """
 
-    ecg_dataframe = pd.read_csv(file)
+    ecg_dataframe = pd.read_csv(file, names=['time', 'voltage'], skiprows=1)
     return ecg_dataframe
 
 if __name__ == "__main__":
-    input_dataframe("ecg_data.csv")
+    input_dataframe()
