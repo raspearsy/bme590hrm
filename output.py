33cf96a49402995b95f1cdf12260af9f9523c952
Mark Palmeri <mlp6@duke.edu>
2017-09-21 10:25:30 -0400

    implement dicts and None conditional

--- output.py
+++ output.py
@@ -1,10 +1,7 @@
-def summarizeECG(instHR, avgHR, brady, tachy):
+def summarizeECG(data):
     """Create txt file summarizing ECG analysis
 
-    :param instHR: (int)
-    :param avgHR: (int)
-    :param brady: (int)
-    :param tachy: (int)
+    :param data: dict{instHR, avgHR, brady, tachy}
     """
     #Calls hrdetector() to get instantaneous heart rate
     #instHR = findInstHR()
@@ -18,9 +15,12 @@ def summarizeECG(instHR, avgHR, brady, tachy):
     
     #Writes the output of the ECG analysis to an output file named ecgOutput.txt
     with open('ecgOutput.txt','w') as ecgResults:
-        instHRstr = "Estimated instantaneous heart rate: {}".format(instHR)
-        avgHRstr = "Estimated average heart rate: {}".format(avgHR)
-        bradystr = "Bradycardia occurred at: {}".format(brady)
-        tachystr = "Tachycardia occurred at: {}".format(tachy)
+        instHRstr = "Estimated instantaneous heart rate: {}".format(data['instHR'])
+        avgHRstr = "Estimated average heart rate: {}".format(data['avgHR'])
+        if data['brady'] is None:
+            bradystr = "Bradycardia never occurred."
+        else:
+            bradystr = "Bradycardia occurred at: {}".format(data['brady'])
+        tachystr = "Tachycardia occurred at: {}".format(data['tachy'])
 
         ecgResults.write(instHRstr + ' BPM\n' + avgHRstr + ' BPM\n' + bradystr + ' sec\n' + tachystr + ' sec')
