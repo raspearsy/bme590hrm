a744823d39952ecb75fc8f25fee995ba84ab6b09
Joshua Khani <jmk@duke.edu>
2017-09-21 16:03:53 -0400

    Find average based on user input

--- output.py
+++ output.py
@@ -1,22 +1,25 @@
+import pandas as pd
+import numpy as np
+
 def summarizeECG(data):
     """Create txt file summarizing ECG analysis
 
     :param data: dict{instHR, avgHR, brady, tachy}
     """
-    #Calls hrdetector() to get instantaneous heart rate
-    #instHR = findInstHR()
 
-    #Calls findAvgHR() to get average heart rate
-    #avgHR = findAvgHR()
-    #Calls bradyTimes() to get times when bradycardia occurred
-    #brady = bradyTimes()
-    #Calls tachtimes() to get times when tachycardia occurred
-    #tachy = tachyTimes()
-    
+    #Get user specified period for averaging heartrate
+    avgPeriod = input("How many minutes to average heartrate?")
+    print("You selected to average over" + avgPeriod + " minutes")
+    #Convert input into index number
+    avgPeriod = (avgPeriod*60)/5
+    #Create column of just the instantaneous heartrates
+    instHR = data['HeartRate']
+    #Average heartrate over period specified by user
+    avgHR = instHR[0:avgPeriod].mean
     #Writes the output of the ECG analysis to an output file named ecgOutput.txt
     with open('ecgOutput.txt','w') as ecgResults:
-        instHRstr = "Estimated instantaneous heart rate: {}".format(data['instHR'])
-        avgHRstr = "Estimated average heart rate: {}".format(data['avgHR'])
+        instHRstr = "Estimated instantaneous heart rate: {}".format(data['HeartRate'])
+        avgHRstr = "Estimated average heart rate after {}: {}".format(avgPeriod,avgHR)
         if data['brady'] is None:
             bradystr = "Bradycardia never occurred."
         else:
