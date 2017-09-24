d05be8a6df6cb6f5ea7e9a47eb97a3a46855d036
Joshua Khani <jmk@duke.edu>
2017-09-24 13:23:33 -0400

    Added start and stop time requests from user

--- output.py
+++ output.py
@@ -7,15 +7,20 @@ def summarizeECG(data):
     :param data: dict{instHR, avgHR, brady, tachy}
     """
 
+    print('This program will summarize the ECG data.\n
+    Enter a start and stop time to get an instantaneous \n
+    heart rate at those time, an average rate over that period, \n
+    and any bradycardia or tachycardia events between those times.')
+
     #Get user specified period for averaging heartrate
-    avgPeriod = input("How many minutes to average heartrate?")
-    print("You selected to average over" + avgPeriod + " minutes")
-    #Convert input into index number
-    avgPeriod = (avgPeriod*60)/5
-    #Create column of just the instantaneous heartrates
-    instHR = data['HeartRate']
-    #Average heartrate over period specified by user
-    avgHR = instHR[0:avgPeriod].mean
+    startTime = input('Start time (in min): ')
+    startTime = (startTime*60)/5
+    stopTime = input('Stop time (in min): ')
+    stopTime = (stopTime*60)/5
+
+    printStr = "ECG data will be summarized from {}s to {}s'".format(startTime,stopTime)
+    print(printStr)
+    
     #Writes the output of the ECG analysis to an output file named ecgOutput.txt
     with open('ecgOutput.txt','w') as ecgResults:
         instHRstr = "Estimated instantaneous heart rate: {}".format(data['HeartRate'])
