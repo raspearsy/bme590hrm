30e493870aa50a58cc116eaff459891d11461a36
Garren Angacian <GarrenA@Garrens-MacBook-Pro.local>
2017-10-16 10:31:50 -0400

    More fixes from Suyash/Dr. Palmeri comments, major missing piece here is adding input arguments to main here so that from command line can input file name, thresholds, etc

--- biomeasure.py
+++ biomeasure.py
@@ -5,7 +5,7 @@ from bioinput import Bioinput
 import sys
 
 
-class Biomeasure:
+class BioMeasure:
     """ This is the main calling class
 
     __init__ sets the __hr_rawdata
@@ -17,6 +17,7 @@ class Biomeasure:
         self.__thr_tachy = thr_tachy
         inputfile = Bioinput()
         self.__hr_rawdata = inputfile.ecg_dataframe
+        self.__data = self.thresholdhr()
 
     def thresholdhr(self):
         """ Will return a list of thresholds, as well as the number of chunks and data_chunk size
@@ -47,17 +48,14 @@ class Biomeasure:
         # Use delta_t, average the HR over that time
         # for i in hr_rawdata[1], find minimum and max locally, threshold on that (using a timing fxn)
         # Calls thresholdhr every so often
-        [thresholds, data_chunk, number_chunks] = self.thresholdhr()
-        # DataFrame, thresholds, data_chunk (# of points per 5 seconds), number_chunks
+        [thresholds, data_chunk, number_chunks] = self.__data
         columns = ['HeartRate', 'B/T', 'time']
-        # index = [None] * len(thresholds)
         hr = pd.DataFrame(numpy.empty(((len(thresholds)), 3)), columns=columns)
         hr['HeartRate'] = None
         hr['B/T'] = 'Healthy... for now'
         hr['time'] = list(range(0, number_chunks * 5, 5))
         hb_count = [0] * len(thresholds)
 
-        # time_step = hr_rawdata['time'][2]-hr_rawdata['time'][1]  # find time step
         for j in range(0, len(thresholds)):
             for i in range(1, data_chunk):
                 if (self.__hr_rawdata['voltage'][i * (j + 1)] > thresholds['Threshold'][j]) and \
@@ -67,15 +65,17 @@ class Biomeasure:
         return hr
 
     def change_threshold(self, threshold):
-        self.__init__(threshold)
+        self.__threshold = threshold
+        # self.__init__(threshold)
 
     def change_brady_threshold(self, brady_threshold):
-        self.__init__(thr_brady=brady_threshold)
+        self.__thr_brady = brady_threshold
 
     def change_tachy_threshold(self, tachy_threshold):
-        self.__init__(thr_tachy=tachy_threshold)
+        self.__thr_tachy = tachy_threshold
 
-    def rhythmdetector(self, hr):
+
+    def detect_rhythm(self, hr):
         """Detects bradycardia & tachycardia based on threshold input and writes instances to hr DataFrame
         """
 
@@ -94,10 +94,11 @@ class Biomeasure:
 
 
 def main(argv):
-    hr_measure = Biomeasure()
+    hr_measure = BioMeasure()
     hr_measure.change_threshold(0.9)
+    hr_measure.change_tachy_threshold(120)
     hr = hr_measure.hrdetector()
-    hr_measure.rhythmdetector(hr)
+    hr_measure.detect_rhythm(hr)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
