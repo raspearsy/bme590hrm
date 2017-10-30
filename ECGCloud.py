34e13d33ce8826fa18911fdb98cf7d97ac1162ea
ras93 <ras93@duke.edu>
2017-10-30 19:09:37 -0400

    wrote the GET function by having the POSTs increment a global counter each time called; likely more elegant solution possible

--- ECGCloud.py
+++ ECGCloud.py
@@ -3,10 +3,21 @@ from ecgmeasure import ECGMeasure
 
 app = Flask(__name__)
 
+# probably not the most elegant way of doing this, but should work
+# more ideal to use something innate to the post to increment the counter
+request_counter = 0
+
+
+@app.route("/api/requests")
+def requests():
+    return jsonify(request_counter)
 
 
 @app.route("/api/heart_rate/summary", methods=['POST'])
 def summary():
+    global request_counter
+    request_counter = request_counter + 1
+
     time = request.json['time']
     voltages = request.json['voltage']
     hr_rawdata = {'time': time, 'voltages': voltages}
@@ -23,23 +34,22 @@ def summary():
 
 @app.route("/api/heart_rate/average", methods=['POST'])
 def average():
-    @app.route("/api/heart_rate/summary", methods=['POST'])
-    def summary():
-        averaging_period = request.json['averaging_period']
-        time = request.json['time']
-        voltages = request.json['voltage']
-        hr_rawdata = {'time': time, 'voltages': voltages}
+    global request_counter
+    request_counter = request_counter + 1
 
-        hr = ECGMeasure(rawdata=hr_rawdata)
-        hr.acquire_avgper(averaging_period=averaging_period)
-        hr.average()
-        hr.detect_rhythm()
-        # NEED TO DEFINE HOW DETECT RHYTHM -- TAKING FROM NEW df INSTEAD
-        # OF from self.data -- for now calling self.avg_data
-        # either add to detect_rhythm *OR* add additional method detect_rhythm_average
+    averaging_period = request.json['averaging_period']
+    time = request.json['time']
+    voltages = request.json['voltage']
+    hr_rawdata = {'time': time, 'voltages': voltages}
 
-        return jsonify(averaging_period=averaging_period,
-                       time_interval=hr.avg_data['time'],
-                       average_heart_rate=hr.avg_data['HeartRate'],
-                       tachycardia_annotations=hr.avg_data['tachycardia_annotations'],
-                       bradycardia_annotations=hr.avg_data['bradycardia_annotations'])
+    hr = ECGMeasure(rawdata=hr_rawdata)
+    hr.acquire_avgper(averaging_period=averaging_period)
+    hr.hrdetector_avg()
+    hr.detect_rhythm_avg()
+    # as of writing, the above methods and below attribute avg_data have not been created
+
+    return jsonify(averaging_period=averaging_period,
+                   time_interval=hr.avg_data['time'],
+                   average_heart_rate=hr.avg_data['HeartRate'],
+                   tachycardia_annotations=hr.avg_data['tachycardia_annotations'],
+                   bradycardia_annotations=hr.avg_data['bradycardia_annotations'])
