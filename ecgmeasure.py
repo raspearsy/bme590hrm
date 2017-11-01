edff3416a5f01f89913ad0c485cfa130865c9d38
Joshua Khani <jmk@duke.edu>
2017-11-01 19:47:31 -0400

    Correct ecgmeasure.py

--- ecgmeasure.py
+++ ecgmeasure.py
@@ -27,7 +27,9 @@ class ECGMeasure:
         self.file = inputfile.file
         self.__hr_rawdata = inputfile.ecg_dataframe
         self.data = None
-        # self.averagingPeriod = inputfile.averagingPeriod
+        self.avgdata = None
+        self.averaging_period = None
+        self.avg_hr = None
 
     def thresholdhr(self):
         """ .. function:: thresholdhr(self)
@@ -64,11 +66,10 @@ class ECGMeasure:
         # Calls thresholdhr every so often
         self.thresholdhr()
         [thresholds, data_chunk, number_chunks] = self.data
-        columns = ['HeartRate', 'bradycardia_annotations', 'tachycardia_annotations', 'time']
-        hr = pd.DataFrame(numpy.empty(((len(thresholds)), 4)), columns=columns)
+        columns = ['HeartRate', 'B/T', 'time']
+        hr = pd.DataFrame(numpy.empty(((len(thresholds)), 3)), columns=columns)
         hr['HeartRate'] = None
-        hr['bradycardia_annotations'] = False
-        hr['tachycardia_annotations'] = False
+        hr['B/T'] = 'Healthy... for now'
         hr['time'] = list(range(0, number_chunks * 5, 5))
         hb_count = [0] * len(thresholds)
 
@@ -125,10 +126,64 @@ class ECGMeasure:
         for x in range(0, len(self.data)):
             if self.data['HeartRate'][x] < self.__thr_brady:
                 bradycount += 1
-                self.data.at[x, 'bradycardia_annotations'] = True
+                self.data.at[x, 'B/T'] = 'Bradycardia Detected'
             elif self.data['HeartRate'][x] > self.__thr_tachy:
                 tachycount += 1
-                self.data.at[x, 'tachycardia_annotations'] = True
+                self.data.at[x, 'B/T'] = 'Tachycardia Detected'
+            else:
+                self.data.at[x, 'B/T'] = 'Healthy... for now'
+
+    def acquire_avgper(self, averaging_period = 5):
+        """.. function:: acquire_avgper(self, averaging_period)
+
+        Adds averaging_period attribute to self
+
+        :param self: instance of ECGMeasure class
+        :param averaging_period: period in seconds for averaging inst hr data
+        """
+        self.averaging_period = averaging_period
+
+    def hrdetector_avg(self):
+        """.. function:: hrdetector_average(self)
+
+        Creates dataframe to contain average data and finds the average for each averaging period
+
+        :param self: instance of ECGMeasure class
+        """
+
+        self.hrdetector()
+        
+        columns = ['HeartRate','time','bradycardia_annotations','tachycardia_annotations']        
+        num_avg_bins = floor(self.data['time'].iat[-1]/self.averaging_period)
+        time_intervals = list(range(1,num_avg_bins+1))
+        
+        avgdata = pd.DataFrame(numpy.empty(((len(time_intervals)),4)), columns = columns)
+        avgdata['time'] = time_intervals
+        avgdata['HeartRate'] = None
+        avgdata['bradycardia_annotations'] = False
+        avgdata['bradycardia_annotations'] = False
+
+        for i in range(0, num_avg_bins):
+            start_ind = int(np.floor(i*averaging_period/5))
+            stop_ind = int(np.floor((i*averaging_period+averaging_period)/5)-1)
+            avgdata.at[i, 'HeartRate'] = self.data['HeartRate'][start_ind:stop_ind].mean()
+
+        self.avgdata = avgdata
+
+    def detect_rhythm_avg(self):
+        """.. function:: detect_rhythm_avg(self)
+
+        Detects bradycardia & tachycardia from average data based on threshold input and writes instances to avgdata DataFrame
+
+        :param self: instance of ECGMeasure class
+        """
+        
+        for x in range(0,len(self.avgdata)):
+            if self.avgdata['HeartRate'][x] < self.__thrbrady:
+                self.avgdata.at[x, 'bradycardia_annotations'] = True
+            elif self.avgdata['HeartRate'][x] >self.__thrtachy:
+                self.avgdata.at[x, 'tachycardia_annotations'] = True
+
 
 
 def main(arguments):
