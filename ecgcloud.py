dc4c61c6ca6e8b99ca3e1ec2bc468aeb79269284
Nisarg Shah <nisarg1shah@gmail.com>
2017-11-03 18:41:03 -0400

    Added error codes for average POST method

--- ecgcloud.py
+++ ecgcloud.py
@@ -54,13 +54,13 @@ def summary():
         hr = ECGMeasure(rawdata=hr_rawdata)
         hr.hrdetector()
         hr.detect_rhythm()
+
+        return jsonify(time=hr.data['time'].tolist(),
+                       instantaneous_heart_rate=hr.data['HeartRate'].tolist(),
+                       tachycardia_annotations=hr.data['tachycardia_annotations'].tolist(),
+                       bradycardia_annotations=hr.data['bradycardia_annotations'].tolist())
     except Exception as e:
         abort(500)
-
-    return jsonify(time=hr.data['time'].tolist(),
-                   instantaneous_heart_rate=hr.data['HeartRate'].tolist(),
-                   tachycardia_annotations=hr.data['tachycardia_annotations'].tolist(),
-                   bradycardia_annotations=hr.data['bradycardia_annotations'].tolist())
 
 
 @app.route("/api/heart_rate/average", methods=['POST'])
@@ -73,22 +73,39 @@ def average():
     global request_counter
     request_counter = request_counter + 1
 
-    averaging_period = request.json['averaging_period']
-    time = request.json['time']
-    voltages = request.json['voltage']
-    hr_rawdata = {'time': time, 'voltage': voltages}
+    try:
+        averaging_period = request.json['averaging_period']
+        time = request.json['time']
+        voltages = request.json['voltage']
+        if len(time) == 0:
+            raise Exception("No input data")
+        if len(time) != len(voltages):
+            raise Exception("Mismatched input data")
+        hr_rawdata = pd.DataFrame.from_dict({'time': time, 'voltage': voltages})
+        hr_rawdata['time'] = pd.to_numeric(hr_rawdata['time'], errors='coerce')
+        hr_rawdata['voltage'] = pd.to_numeric(hr_rawdata['voltage'], errors='coerce')
+        hr_rawdata = hr_rawdata.dropna(axis=0, how="any")
+        hr_rawdata = hr_rawdata[abs(hr_rawdata['voltage']) <= 300]
+        hr_rawdata = hr_rawdata.reset_index()
+        if len(hr_rawdata) == 0 or type(averaging_period) is not int:
+            raise Exception("No valid input data")
+    except Exception as e:
+            abort(400, str(e))
 
-    hr = ECGMeasure(rawdata=hr_rawdata)
-    hr.acquire_avgper(averaging_period=averaging_period)
-    hr.hrdetector_avg()
-    hr.detect_rhythm_avg()
-    # as of writing, the above methods and below attribute avg_data have not been created
+    try:
+        hr = ECGMeasure(rawdata=hr_rawdata)
+        hr.acquire_avgper(averaging_period=averaging_period)
+        hr.hrdetector_avg()
+        hr.detect_rhythm_avg()
+        # as of writing, the above methods and below attribute avg_data have not been created
 
-    return jsonify(averaging_period=averaging_period,
-                   time_interval=hr.avg_data['time'].tolist(),
-                   average_heart_rate=hr.avg_data['HeartRate'].tolist(),
-                   tachycardia_annotations=hr.avg_data['tachycardia_annotations'].tolist(),
-                   bradycardia_annotations=hr.avg_data['bradycardia_annotations'].tolist())
+        return jsonify(averaging_period=averaging_period,
+                       time_interval=hr.avg_data['time'].tolist(),
+                       average_heart_rate=hr.avg_data['HeartRate'].tolist(),
+                       tachycardia_annotations=hr.avg_data['tachycardia_annotations'].tolist(),
+                       bradycardia_annotations=hr.avg_data['bradycardia_annotations'].tolist())
+    except Exception as e:
+        abort(500)
 
 
 @app.errorhandler(400)
